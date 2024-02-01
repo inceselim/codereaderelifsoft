5ba42f81a8731afda6c6e6c11887336bec3dadda
inceselim <inceselim91@gmail.com>
2024-02-01 16:53:01 +0300

    01.02.24-Ariza Kaydı Add Malzeme Done

--- src/api/api_url.js
+++ src/api/api_url.js
@@ -10,5 +10,6 @@ export const API_URL = {
     ARIZA_MALZEME_LISTELE: "/api/ArizaApi/LogoMaterialList",
     ARIZA_MALZEME_EKLE: "/api/ArizaApi/ArizaMalzemeAdd",
     ARIZA_MALZEME_SIL: "/api/ArizaApi/ArizaMalzemeDelete?key=6556773",
-    ARIZA_TAMIRCI_LIST: "/api/ArizaApi/ArizaMalzemeTamirciList"
+    ARIZA_TAMIRCI_LIST: "/api/ArizaApi/ArizaMalzemeTamirciList",
+    ARIZA_GONDER_URL: "/api/ArizaApi/ArizaMalzemeAdd"
 }
--- src/screens/ariza/ArizaMalzemeEkleScreen.tsx
+++ src/screens/ariza/ArizaMalzemeEkleScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, SafeAreaView, Pressable, Modal, StyleSheet, Alert, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, SafeAreaView, Pressable, Modal, StyleSheet, Alert, TextInput, TouchableOpacity, FlatList, ScrollView } from 'react-native';
 import { styles } from '../../styles/styles';
 import { colors } from '../../styles/colors';
 import ButtonPrimary from '../../components/ButtonPrimary/ButtonPrimary';
@@ -9,6 +9,7 @@ import { useSelector } from 'react-redux';
 import LoadingCard from '../../components/LoadingCard/LoadingCard';
 import { styleModal } from '../../styles/styleModal';
 import { CloseCircle } from 'iconsax-react-native';
+import CardView from '../../components/CardView';
 
 export default function ArizaMalzemeEkleScreen({ props, route }: any) {
   const userToken = useSelector((state: any) => state.auth?.userToken)
@@ -19,7 +20,8 @@ export default function ArizaMalzemeEkleScreen({ props, route }: any) {
 
   const [loadingMalzeme, setLoadingMalzeme] = useState(false);
   const [loadingTamirci, setLoadingTamirci] = useState(false);
-  const [dataMalzeme, setDataMalzeme] = useState([]);
+  const [loadingGonder, setLoadingGonder] = useState(false);
+  const [dataMalzeme, setDataMalzeme] = useState<any>([]);
   const [selectedMalzeme, setSelectedMalzeme] = useState<any>([]);
   const [dataTamirci, setDataTamirci] = useState<any>([]);
   const [visibleTamirci, setVisibleTamirci] = useState(false);
@@ -40,6 +42,7 @@ export default function ArizaMalzemeEkleScreen({ props, route }: any) {
       .then((response: any) => {
         // console.log("MALZEME data: ", response.data);
         setDataMalzeme(response.data)
+        console.log("dataMalzeme", dataMalzeme)
       })
       .catch((err: any) => {
         console.log("HATA ariza malzeme ara: ", err)
@@ -66,105 +69,84 @@ export default function ArizaMalzemeEkleScreen({ props, route }: any) {
       })
       .finally(() => setLoadingTamirci(false))
   }
+
+  async function handleData() {
+    console.log("TAMAM")
+    setLoadingGonder(true);
+    await axios.post(API_URL.BASE_URL + API_URL.ARIZA_GONDER_URL, {}, {
+      headers: {
+        "Authorization": "Bearer " + userToken
+      }
+    })
+      .then((response: any) => {
+        console.log("MAL GONDER RES: ", response.data)
+      })
+      .catch((error: any) => console.log("ERROR: ", error))
+      .finally(() => setLoadingGonder(false))
+  }
   useEffect(() => {
     getTamirciList()
   }, [])
   return (
     <SafeAreaView style={styles.container}>
       {
-        loadingMalzeme || loadingTamirci ?
+        loadingMalzeme || loadingTamirci || loadingGonder ?
           <LoadingCard />
           :
-          <View style={styles.content}>
-            {
-              !visibleTamirci ?
-                <>
-                  <ButtonPrimary text={"Tamirci Seç"}
-                    onPress={() => {
-                      setVisibleTamirci(true)
-                    }} />
-                  <Text style={styles.textBold}>Tamirci</Text>
-                  <Text style={styles.textSmall}>{selectedTamirci?.fullName}</Text>
-                </>
-                :
-                <FlatList data={dataTamirci}
-                  renderItem={({ item }: any) => {
+          <ScrollView>
+            <View style={styles.content}>
+              <CardView>
+                <TextInput style={[styles.textInput, {
+                  backgroundColor: colors.white,
+                  marginVertical: 8,
+                  borderRadius: 8,
+                  borderColor: colors.gray,
+                  borderWidth: 1,
+                  paddingStart: 12,
+                  fontSize: 15,
+                  fontWeight: "normal"
+                }]}
+                  focusable={true}
+                  placeholder='Barkod & Malzeme Adı Giriniz...'
+                  placeholderTextColor={colors.gray}
+                  value={barkod}
+                  onChangeText={setBarkod}
+                  autoFocus
+                />
+                <ButtonPrimary text={"Malzeme Ara"} onPress={() => getMalzeme()} disabled={barkod != "" ? false : true} />
+                {
+                  dataMalzeme?.map((item: any) => {
                     return (
-                      <View>
-                        <TouchableOpacity style={{ paddingVertical: 4, }} onPress={() => {
-                          setSelectedTamirci(item)
-                          setVisibleTamirci(false)
-                        }}>
-                          <Text style={styles.textSmall}>{item?.fullName}</Text>
-                        </TouchableOpacity>
+                      <View key={item?.Code}>
+                        <Text style={styles.textBold}>{item?.Code}</Text>
+                        <View style={styles.viewTwoRowJustify}>
+                          <Text style={styles.textNormal}>Stok Adet:</Text>
+                          <Text style={styles.textNormal}>{item?.Onhand} {item?.UnitName}</Text>
+                        </View>
+                        <Text style={styles.textNormal}>{item?.Name}</Text>
                       </View>
                     )
-                  }}
-                />
-            }
-            <TextInput style={[styles.textInput, {
-              backgroundColor: colors.white,
-              marginVertical: 8,
-              borderRadius: 8,
-              borderColor: colors.gray,
-              borderWidth: 1,
-              paddingStart: 12,
-              fontSize: 15,
-              fontWeight: "normal"
-            }]}
-              focusable={true}
-              placeholder='Barkod & Malzeme Adı Giriniz...'
-              placeholderTextColor={colors.gray}
-              value={barkod}
-              onChangeText={setBarkod}
-              autoFocus
-            />
-            <ButtonPrimary text={"Malzeme Ara"} onPress={() => getMalzeme()} disabled={barkod != "" ? false : true} />
-            {
-              dataMalzeme?.map((item: any) => {
-                return (
-                  <View key={item?.Code}>
-                    <Text style={styles.textBold}>{item?.Code}</Text>
-                    <View style={styles.viewTwoRowJustify}>
-                      <Text style={styles.textNormal}>Stok Adet:</Text>
-                      <Text style={styles.textNormal}>{item?.Onhand} {item?.UnitName}</Text>
-                    </View>
-                    <Text style={styles.textNormal}>{item?.Name}</Text>
-                  </View>
-                )
-              })
-            }
-            {/* <Modal
-              animationType="slide"
-              transparent={true}
-              visible={visibleTamirci}
-              onRequestClose={() => {
-                setVisibleTamirci(!visibleTamirci);
-              }}>
-              <View style={styleModal.centeredView}>
-                <View style={styleModal.modalView}>
-                  <View style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    paddingBottom: 6,
-                    borderBottomWidth: 1,
-                    marginBottom: 4,
-                  }} >
-                    <Text style={{
-                      fontSize: 17,
-                      fontWeight: "bold",
-                      color: colors.primaryColor,
-                    }}>Tamirci Seç</Text>
-                    <TouchableOpacity onPress={() => setVisibleTamirci(!visibleTamirci)}>
-                      <CloseCircle size="32" color={colors.black} />
-                    </TouchableOpacity>
-                  </View>
+                  })
+                }
+              </CardView>
+
+
+              {
+                !visibleTamirci ?
+                  <CardView>
+                    <ButtonPrimary text={"Tamirci Seç"}
+                      onPress={() => {
+                        setVisibleTamirci(true)
+                      }} />
+                    <Text style={styles.textBold}>Tamirci</Text>
+                    <Text style={styles.textSmall}>{selectedTamirci?.fullName}</Text>
+                  </CardView>
+                  :
                   <FlatList data={dataTamirci}
                     renderItem={({ item }: any) => {
                       return (
                         <View>
-                          <TouchableOpacity onPress={() => {
+                          <TouchableOpacity style={{ paddingVertical: 4, }} onPress={() => {
                             setSelectedTamirci(item)
                             setVisibleTamirci(false)
                           }}>
@@ -174,22 +156,29 @@ export default function ArizaMalzemeEkleScreen({ props, route }: any) {
                       )
                     }}
                   />
-                </View>
-              </View>
-            </Modal> */}
+              }
 
-            <TextInput style={styles.textInput}
-              placeholder='Malzeme Adet'
-              inputMode='decimal'
-              value={malzemeAdet}
-              onChangeText={setMalzemeAdet}
-              placeholderTextColor={"#555"} />
-            <ButtonPrimary text={"Kaydet"}
-              disabled={
-                (malzemeAdet != "") &&
-                  (selectedTamirci != "") &&
-                  (selectedMalzeme != "") ? false : true}></ButtonPrimary>
-          </View>
+              <TextInput style={styles.textInput}
+                placeholder='Malzeme Adet'
+                inputMode='decimal'
+                value={malzemeAdet}
+                onChangeText={setMalzemeAdet}
+                placeholderTextColor={"#555"} />
+              <ButtonPrimary text={"Kaydet"}
+                onPress={() => {
+                  parseInt(dataMalzeme[0].Onhand) < parseInt(malzemeAdet) ?
+                    // console.log("Onhand", dataMalzeme[0].Onhand)
+                    Alert.alert("Hata", "Stok Yeterli Değil")
+                    :
+                    // console.log("object", (dataMalzeme[0].Onhand))
+                    handleData()
+                }}
+                disabled={
+                  (malzemeAdet != "") &&
+                    (selectedTamirci != null) &&
+                    (dataMalzeme != null) ? false : true}></ButtonPrimary>
+            </View>
+          </ScrollView>
       }
     </SafeAreaView>
   );
